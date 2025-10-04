1825b7bb6996361d9d481858b0a1f44451d69a7f
varun-gokte <vsgokte@gmail.com>
2025-10-04 19:16:37 +0530

    added selection dropdown

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,25 +11,38 @@ from sentence_transformers import SentenceTransformer, util
 @main.route('/', methods=['POST','GET'])
 def home():
     if request.method=='POST':
-        if 'file' not in request.files or 'description' not in request.form:
+        print (request.form)
+        if not('file' in request.files or "existingFile" in request.form) or 'description' not in request.form:
             return jsonify({'error':'Missing fields'}),400
-        file = request.files['file']
-        if file.filename=='':
-            return jsonify({'error':'No file selected'}),400
+        if "existingFile" in request.form:
+            resume_id = request.form["existingFile"]
+            text = Resume.query.get(resume_id).parsed_text
+            print ('text',text)
+        else:
+            file = request.files['file']
+            if file.filename=='':
+                return jsonify({'error':'No file selected'}),400
+            filename = f'{int(time.time())}{secure_filename(file.filename)}'
+            filepath = os.path.join(current_app.config['UPLOAD_FOLDER'],filename)
+            file.save(filepath)
+            text = ""
+            with pdfplumber.open(filepath) as pdf:
+                for page in pdf.pages:
+                    page_text = page.extract_text()
+                    if page_text:
+                        text += page_text + "\n"
+            uploaded_resume = Resume(
+                filename = filename,
+                filepath = filepath,
+                mimetype = file.mimetype,
+                size = os.path.getsize(filepath),
+                parsed_text = text
+            )
+            db.session.add(uploaded_resume)
+
         description = request.form['description']
         title = request.form['job_title']
-        company = request.form['company']
-
-        filename = f'{int(time.time())}{secure_filename(file.filename)}'
-        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'],filename)
-        file.save(filepath)
-
-        text = ""
-        with pdfplumber.open(filepath) as pdf:
-            for page in pdf.pages:
-                page_text = page.extract_text()
-                if page_text:
-                    text += page_text + "\n"
+        company = request.form['company']       
 
         model = SentenceTransformer('all-MiniLM-L6-v2')
         resume_embeddings = model.encode(text, convert_to_tensor=True)
@@ -38,14 +51,6 @@ def home():
         similarity = util.cos_sim(resume_embeddings, job_desc_embeddings)
 
         score = similarity.item()
-
-        uploaded_resume = Resume(
-            filename = filename,
-            filepath = filepath,
-            mimetype = file.mimetype,
-            size = os.path.getsize(filepath),
-            parsed_text = text
-        )
         
         job_details = Job(
             title=title,
@@ -53,7 +58,6 @@ def home():
             company=company
         )
 
-        db.session.add(uploaded_resume)
         db.session.add(job_details)
 
         db.session.commit()
@@ -61,4 +65,9 @@ def home():
         return render_template('index.html', score=score)
         
     else:
-        return render_template('index.html')
+        resumes = Resume.query.all()
+        print (resumes)
+        res = []
+        for resume in resumes:
+            res.append({"id":resume.id, "filename":resume.filename})
+        return render_template('index.html',resumeList=res)
