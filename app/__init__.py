3f6bee823d5e3da028a8803b2861c288b2d7ea10
varun-gokte <vsgokte@gmail.com>
2025-10-07 18:45:58 +0530

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,12 @@ db_username = os.getenv('DB_USERNAME')
 db_password = os.getenv('DB_PASSWORD')
 db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
+db_host = os.getenv('DB_HOST')
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_username}:{db_password}@127.0.0.1:{db_port}/{db_name}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR,'uploads')
